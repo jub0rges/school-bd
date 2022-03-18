f54a94e70cfbfa89a6c76c4bc2cc4b1d292e1233
Julia Rodrigues Borges <101815436+jub0rges@users.noreply.github.com>
2022-03-18 18:00:23 -0300

    Add files via upload

---
+++ MySQL/SQL Dados-tab.sql
@@ -0,0 +1,43 @@
+﻿
+
+Insert into tb_cliente (CodCli, CliName, CliCnh, CliAdress) values
+(1, 'Lexie', '12568954785', 'João Navarro, 53'),
+(2, 'Omalley', '85462059782', 'Bus, 256'),
+(3, 'Balley', '65895126423', 'Fourth, 458'),
+(4, 'Meridit', '85426514632', 'Solid, 598'),
+(5, 'Derick', '95862431052', 'Died, 665'),
+(6, 'Max Sloan', '84596521535', 'Colômbia, 0001'),
+(7, 'Yang', '78459623105', 'Crazy Lima , 235'),
+(8, 'Karev', '84659235410', 'Putraya, 69'),
+(9, 'Izzie', '98564125632', 'Câncer, 548'),
+(10, 'April', '546218915354', 'Vergente, 776');
+
+Select CodCli, CliName, CliCnh, CliAdress from tb_cliente
+
+insert tb_aluga (CodAlu, DataInicio, DataFim) values
+
+(1, '2022-03-30', '2022-10-22'),
+(2, '2020-02-09', '2022-10-30'),
+(3, '2021-01-20', '2022-10-05'),
+(4, '2020-05-09', '2022-10-07'),
+(5, '2021-07-13', '2022-10-08'),
+(6, '2013-08-10', '2016-10-23'),
+(7, '2019-05-05', '2021-10-25'),
+(8, '2015-07-03', '2021-10-19'),
+(9, '2017-09-29', '2022-10-28'),
+(10, '2019-10-28', '2020-10-17');
+
+Select CodAlu, DataInicio, DataFim from tb_aluga
+
+insert tb_carro (CodCar, Placa ,Cor, Modelo ,Ano ,Diaria) values
+
+(1, 'FOA-5689', 'vermelho', 'Celta', '2018', '20,25'),
+(2, 'JUU-3003', 'azul', 'Fox', '2005', '70,25'),
+(3, 'TAY-9658', 'verde', 'Cobalt', '2022', '200,26'),
+(4, 'DEX-6949', 'preto', 'Palio', '2015', '259,20'),
+(5, 'BMW-5689', 'branco', 'Corsa', '2017', '2158,05'),
+(6, 'VFG-3526', 'cinza', 'Uno', '2003', '215,56'),
+(7, 'KOU-9145', 'vinho', 'Siena', '2005', '254,02'),
+(8, 'LOU-5264', 'amarelo', 'Voyage', '2021', '26,35'),
+(9, 'TRG-0215', 'rosa', 'Hb20', '2018', '25,69'),
+(10, 'ANI-2005', 'branco', 'Onix', '2009', '200,25');
---
+++ MySQL/SQL-carros.sql
@@ -0,0 +1,38 @@
+﻿create database aluguel_carros
+
+
+
+use aluguel_carros
+
+
+
+create table tb_Cliente
+(
+CodCli int primary key,
+CliName Varchar (50),
+CliCnh Varchar (11),
+CliAdress Varchar (100)
+);
+
+
+
+create table tb_Aluga
+(
+CodAlu int primary key,
+DataInicio date,
+DataFim date,
+CodCar integer,
+CodCli integer
+);
+
+
+
+create table tb_Carro
+(
+CodCar int primary key,
+Placa Varchar (8),
+Cor Varchar (15),
+Modelo Varchar (20),
+Ano integer,
+Diaria numeric(4,2)
+);
